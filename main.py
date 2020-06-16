b27c77f20c2ee34ff0da2f8f05640dea9c57dba7
BearBug7 <niamhj577@gmail.com>
2020-06-16 17:51:08 +0000

    Add the winning the match functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np 
-from random import*
+from random import *
 
 rows = 4
 cols = 4
@@ -7,14 +7,13 @@ board = np.zeros((rows, cols, 2))
 characters = [u'\u0293',u'\u0278',u'\u03C0',u'\u03A9',  u'\u03A8',u'\u03A3',u'\u0414',u'\u0411']
 
 def make_board():
-  print("here")
   for i in range(2):
     for charid in range(len(characters)):
       r = randint(0, rows-1)
       c = randint(0, rows-1)
       while board[r, c, 1] != 0.0:
         r = randint(0, rows-1)
-        c + randint(0, rows-1)
+        c = randint(0, rows-1)
       board[r, c, 1] = charid
 
 def print_board():
@@ -31,18 +30,51 @@ def get_choice(limit, pos, turn):
     except ValueError:
       print("That is not a number. Please enter a number between 1 and {}.".format(limit))
     else: 
-      if id > limit or id < limit:
+      if id >= limit:
         print("That {} is out of range. Please enter a number that is between 1 and {}".format(pos, limit))
       else:
         break
 
-
-
-
+  return id
 
 def main():
   make_board()
   print_board()
+  match_count = 0
+  attempts = 8
+  first_flip = True
+  while match_count < len(characters):
+    if first_flip:
+      r1 = get_choice(rows, "row", "first")
+      c1 = get_choice(cols, "column", "first")
+      if board[r1, c1, 0] == 1.0:
+        print("That card is already flipped. Please try again.")
+      else:
+        first_flip = False
+        board[r1, c1, 0] = 1.0
+        print_board()
+    # flip no.2
+    else:
+      r2 = get_choice(rows, "row", "second")
+      c2 = get_choice(rows, "column", "second")
+      if board[r2, c2, 0] == 1.0:
+        print("That card is already flipped.")
+      else:
+        attempts += 1
+        first_flip = True
+        board[r2, c2, 0] = 1.0
+        print_board()
+        if board[r1, c1, 1] == board[r2, c2, 1]:
+          print("You got a match!")
+          match_count += 1
+        else: 
+          print("No match! Better luck next time.")
+          board[r1, c1, 0] = 0.8
+          board[r2, c2, 0] = 0.8
+          print_board()
+  print("You completed the game in {} attempts!".format(attempts))
+
+
 
 
 if __name__== '__main__':
